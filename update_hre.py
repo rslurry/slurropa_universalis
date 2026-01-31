4b3ed66d353eb92de026dc5290b6b6902537119f
rslurry <112521174+rslurry@users.noreply.github.com>
2026-01-31 00:26:45 -0500

    Enable making republics HRE electors

--- update_hre.py
+++ update_hre.py
@@ -6,26 +6,46 @@ import re
 
 load_dotenv()
 
-FHRE_ELECTOR = os.path.join("in_game", "common", "international_organization_special_statuses", "hre.txt")
-FINPUT_ELECTOR  = os.path.join(os.getenv("EU5_GAME_FOLDER"), FHRE_ELECTOR)
-FOUTPUT_ELECTOR = os.path.join(os.getenv("SLURROPA_FOLDER"), FHRE_ELECTOR)
+FHRE_SPECSTATUS = os.path.join("in_game", "common", "international_organization_special_statuses", "hre.txt")
+FINPUT_SPECSTATUS  = os.path.join(os.getenv("EU5_GAME_FOLDER"), FHRE_SPECSTATUS)
+FOUTPUT_SPECSTATUS = os.path.join(os.getenv("SLURROPA_FOLDER"), FHRE_SPECSTATUS)
 
-with open(FINPUT_ELECTOR, 'r') as foo:
-    hre_elector_data = foo.read()
+with open(FINPUT_SPECSTATUS, 'r') as foo:
+    hre_specstatus_data = foo.read()
 
 # Regex to target ONLY the line inside can_bestow_trigger
-elector_pattern = r'(can_bestow_trigger\s*=\s*{[^}]*?)government_type\s*=\s*government_type:monarchy'
+elector_pattern = r'(can_bestow_trigger\s*=\s*{.*?\n)(\s*)government_type\s*=\s*government_type:monarchy'
 
-elector_replacement = r"""\1OR = {
-        government_type = government_type:monarchy
-        government_type = government_type:republic
-    }"""
+elector_replacement = r"""\1\2OR = {
+\2\tgovernment_type = government_type:monarchy
+\2\tgovernment_type = government_type:republic
+\2}"""
 
-hre_elector_data = re.sub(elector_pattern, elector_replacement, hre_elector_data, flags=re.DOTALL)
+hre_specstatus_data = re.sub(elector_pattern, elector_replacement, hre_specstatus_data, flags=re.DOTALL)
 
-if not os.path.exists(os.path.dirname(FOUTPUT_ELECTOR)):
-    os.makedirs(os.path.dirname(FOUTPUT_ELECTOR))
+rescind_pattern = r'(\s*)NOT\s*=\s*{\s*government_type\s*=\s*government_type:monarchy\s*}'
+rescind_replacement = r"""\1NOT = {
+\1\tOR = {
+\1\t\tgovernment_type = government_type:monarchy
+\1\t\tgovernment_type = government_type:republic
+\1\t}
+\1}"""
 
-with open(FOUTPUT_ELECTOR, 'w') as foo:
-    foo.write(hre_elector_data)
+rescind_pattern = r'\n(\s*)NOT\s*=\s*{\s*government_type\s*=\s*government_type:monarchy\s*}\n'
+rescind_replacement = (
+    r"\n\1NOT = {\n"
+    r"\1\tOR = {\n"
+    r"\1\t\tgovernment_type = government_type:monarchy\n"
+    r"\1\t\tgovernment_type = government_type:republic\n"
+    r"\1\t}\n"
+    r"\1}\n"
+)
+
+hre_specstatus_data = re.sub(rescind_pattern, rescind_replacement, hre_specstatus_data)
+
+if not os.path.exists(os.path.dirname(FOUTPUT_SPECSTATUS)):
+    os.makedirs(os.path.dirname(FOUTPUT_SPECSTATUS))
+
+with open(FOUTPUT_SPECSTATUS, 'w') as foo:
+    foo.write(hre_specstatus_data)
 
